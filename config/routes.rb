7a0ee4893cc545352ad28c7d856fcc981419ab8b
JoeMugwaneza <muganezajoseph@gmail.com>
2017-05-11 18:56:44 +0200

    created sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :comments
 
   resources :replies
+
+  get "/admin", to: 'sessions#new'
+  post "/admin", to: 'sessions#create'
+  delete "/admin_out", to: 'sessions#destroy'
 end
