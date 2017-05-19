3296a53371eac640bda2d4ab7d32bedbaba22b90
Jean Luc Abayo <luc.bayo@gmail.com>
2017-05-19 23:59:03 +0200

    added some gems

---
+++ db/migrate/20170516180838_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions do |t|
+      t.string :name
+      t.string :email
+      t.text :body
+      t.string :web
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511154816) do
+ActiveRecord::Schema.define(version: 20170516180838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20170511154816) do
     t.string   "title"
     t.text     "body"
     t.text     "intro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.string   "web"
+    t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
