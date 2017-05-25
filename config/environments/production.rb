1b82cd2d0e4104b8f6eb5a7c0a8d2f81d517b07d
Jean Luc Abayo <luc.bayo@gmail.com>
2017-05-25 18:07:26 +0200

    modified production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,13 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "nexpals_#{Rails.env}"
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:'smtp.gmail.com',
@@ -68,10 +73,6 @@ Rails.application.configure do
       enable_starttls_auto: true  }
 
   config.action_mailer.default_url_options = { :host => 'nexpals.herokuapp.com' }
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
