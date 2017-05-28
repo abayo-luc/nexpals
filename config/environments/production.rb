ed1c6ca4db49c87ee9f7882a569dc93796283593
Jean Luc Abayo <luc.bayo@gmail.com>
2017-05-29 00:04:26 +0200

    modified production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
       port: 587,
       domain: 'gmail.com',
       user_name: ENV['EMAIL'],
-      password: ENV['SMTP_PASSWORD'],
+      password: "nexpals250@gmail.com",
       authentication:'plain',
       enable_starttls_auto: true  }
 
