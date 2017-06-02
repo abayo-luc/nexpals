c6760b2799c6fe3f1fe476ac2fe041be58052b74
plantejosue <plantejosh1@gmail.com>
2017-06-02 14:29:51 +0200

    resolved post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
     find_post
     @comment = Comment.new
     @reply = Reply.new
-
+    @posts = Post.paginate(:page => params[:page], :per_page => 4).order(created_at: "DESC")
     # impressionist(@post)
   end
 
