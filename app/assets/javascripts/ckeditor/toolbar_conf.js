973424a8b0995624aa4e6d6f8ad35774387df50a
Jean Luc Abayo <luc.bayo@gmail.com>
2017-06-20 15:27:54 +0200

    fixed image responsiviness on ckeditor

--- app/assets/javascripts/ckeditor/toolbar_conf.js
+++ app/assets/javascripts/ckeditor/toolbar_conf.js
@@ -1,8 +1,8 @@
 CKEDITOR.editorConfig = function( config )
 {
   // Define changes to default configuration here. For example:
-  // config.language = 'fr';
-  // config.uiColor = '#AADC6E';
+  config.language = 'en';
+  config.uiColor = '#AADC6E';
 
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
