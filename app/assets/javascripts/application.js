49684eff518d56861e6a47955296d4097d4d4d2b
Jean Luc Abayo <luc.bayo@gmail.com>
2017-07-14 01:09:07 +0200

    moved bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks 
+//= require bootstrap-sprockets
 //= require ckeditor/init
 //= require social-share-button
 //= require turbolinks
